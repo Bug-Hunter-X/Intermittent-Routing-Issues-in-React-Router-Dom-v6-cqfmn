0fc016653f61254d882f7c6d78057c215a9eadbc
BugHunter-X <askzenai@gmail.com>
2025-01-23 11:52:07 +0100

    Add AppSolution.js file

---
+++ AppSolution.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <BrowserRouter>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+//Home, About and Contact components
